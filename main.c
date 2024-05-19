65939bd9547de474239701f69f07fdfec828a895
belhadj haitam <steankir.haitam@gmail.com>
2024-05-19 21:02:33 +0100

    add:

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 19:58:34 by aatbir            #+#    #+#             */
-/*   Updated: 2024/05/19 11:28:40 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/05/19 21:02:04 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ int	main(int ac, char **av)
 	str = store_get_info_in_array(info, av[1]);
 	// while(str[i])
 	// 	printf("%s\n", str[i++]);
+	printf ("%c\n", info_path->info->pos);
 	// info->first_color = 0;
 	// info->first_info = 0;
 	// info
